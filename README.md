356c6fb8b13a5ab5b29d7d2ac59e377f925754cb
salty-muffin <t.babel@gmx.com>
2024-07-13 18:12:28 +0200

    added capture

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # memory-generator
+
 A collection of scripts for generating future memories of the now
+
+## requirements
+
+python >= 3.10.x
---
+++ capture.py
@@ -0,0 +1,80 @@
+import os
+import click
+from dotenv import dotenv_values
+import cv2
+import time
+
+
+def capture_and_process_frame(cap: cv2.VideoCapture, max_width=512):
+    # capture frame from webcam
+    ret, frame = cap.read()
+
+    if not ret:
+        return None, None
+
+    # get current dimensions
+    height, width = frame.shape[:2]
+
+    # calculate new dimensions
+    if width > max_width:
+        ratio = max_width / width
+        new_width = max_width
+        new_height = int(height * ratio)
+        frame = cv2.resize(frame, (new_width, new_height))
+
+    return frame
+
+
+# fmt: off
+@click.command()
+@click.option("--capture", "-c", type=int, default=0, help="the index of the capture device to use")
+@click.argument("directory", type=click.Path(exists=False))
+# fmt: on
+def capture(capture: int, directory: str) -> None:
+    # initialize the webcam
+    capture = cv2.VideoCapture(capture)
+
+    # check if the webcam is opened correctly
+    if not capture.isOpened():
+        print("cannot open webcam")
+        raise SystemExit
+
+    # create the directory in which to store the frame
+    os.makedirs(directory, exist_ok=True)
+
+    print("starting capture in 5 seconds. close window with 'q' or close in terminal")
+    try:
+        # wait for the camera to initialize and adjust light levels
+        time.sleep(5)
+
+        cv2.namedWindow("preview")
+
+        while True:
+            frame = capture_and_process_frame(capture)
+
+            if frame is not None:
+                # display the captured frame
+                cv2.imshow("preview", frame)
+
+                # save the captured frame
+                filename = "frame.jpg"
+                cv2.imwrite(os.path.join(directory, filename), frame)
+
+                # wait for 3 seconds or until 'q' is pressed
+                if cv2.waitKey(1000) & 0xFF == ord("q"):
+                    raise KeyboardInterrupt
+            else:
+                print("failed to capture frame")
+                break
+
+    except KeyboardInterrupt:
+        print("closing...")
+    except Exception as e:
+        repr(e)
+    finally:
+        cv2.destroyAllWindows()
+        capture.release()
+
+
+if __name__ == "__main__":
+    capture()
